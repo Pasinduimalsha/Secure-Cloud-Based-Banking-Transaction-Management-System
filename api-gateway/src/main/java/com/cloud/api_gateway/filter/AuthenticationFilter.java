a41f99a4247bad469dc9ddec2ac631e2a55fdbb3
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2026-04-23 00:14:52 +0530

    added gateway changes

--- api-gateway/src/main/java/com/cloud/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/cloud/api_gateway/filter/AuthenticationFilter.java
@@ -1,28 +1,19 @@
 package com.cloud.api_gateway.filter;
 
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.function.HandlerFilterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @Component
-public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
-
-    public AuthenticationFilter() {
-        super(Config.class);
-    }
+public class AuthenticationFilter implements HandlerFilterFunction<ServerResponse, ServerResponse> {
 
     @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            // TODO: In the future, extract JWT token from exchange.getRequest().getHeaders()
-            // Validate the token and route or reject.
-            
-            // For now, act as a generic pass-through.
-            return chain.filter(exchange);
-        };
-    }
-
-    public static class Config {
-        // Put configuration properties here
+    public ServerResponse filter(ServerRequest request, org.springframework.web.servlet.function.HandlerFunction<ServerResponse> next) throws Exception {
+        // TODO: In the future, extract JWT token from request.headers()
+        // Validate the token and route or reject.
+        
+        // For now, act as a generic pass-through.
+        return next.handle(request);
     }
 }
