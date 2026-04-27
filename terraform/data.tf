3154cc14bd083742ef77867c1fa6a065edd3a7e2
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2026-04-27 17:19:07 +0530

    fixed availability zone issue

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,4 +7,10 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+
+  # Filter out AZs that don't support m7i-flex instances (like us-east-1e)
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
 }
